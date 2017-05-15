0bbaa30a826246081db1086bae00882a4f907634
Ben Wu <wubin28@gmail.com>
2017-05-15 14:08:58 +0800

    the_anagram_of_an_empty_string_should_be_itself

---
+++ src/main/java/kata/Anagrams.java
@@ -0,0 +1,13 @@
+package kata;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by twer on 5/15/17.
+ */
+public class Anagrams {
+    public static List<String> of(String input) {
+        return Arrays.asList("");
+    }
+}
---
+++ src/test/java/kata/AnagramsTest.java
@@ -0,0 +1,15 @@
+package kata;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AnagramsTest {
+    @Test
+    public void the_anagram_of_an_empty_string_should_be_itself() {
+        assertThat(Anagrams.of(""), is(Arrays.asList("")));
+    }
+}
