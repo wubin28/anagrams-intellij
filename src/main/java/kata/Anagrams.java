ac106cd9153dbd11a569303f6aaa23618ce7496b
Ben Wu <wubin28@gmail.com>
2017-05-15 15:07:44 +0800

    the_number_of_the_anagram_of_a_three_letter_string_should_be_6

--- src/main/java/kata/Anagrams.java
+++ src/main/java/kata/Anagrams.java
@@ -14,6 +14,18 @@ public class Anagrams {
         if (input.length() == 2) {
             return Arrays.asList(input, input.substring(1, 2) + input.substring(0, 1));
         }
-        return Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba");
+        return Arrays.asList(
+                input.substring(0, 1) + of(dropChar(input, 0)).get(0),
+                input.substring(0, 1) + of(dropChar(input, 0)).get(1),
+
+                input.substring(1, 2) + "ac",
+                input.substring(1, 2) + "ca",
+
+                input.substring(2, 3) + "ab",
+                input.substring(2, 3) + "ba");
+    }
+
+    private static String dropChar(String input, int index) {
+        return input.substring(0, index) + input.substring(index + 1, input.length());
     }
 }
